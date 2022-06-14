192f1f1e3c1a91dcaaea74a9fb1ab4d3f7b40b2e
DanielOfRivia <daniloustimenko@gmail.com>
2022-06-14 16:32:45 +0300

    Lab_1 Ustymenko Part 2

--- Lab_1/src/test/java/JUnitTest.java
+++ Lab_1/src/test/java/JUnitTest.java
@@ -1,4 +1,7 @@
 
+import classes.Message;
+import classes.Packet;
+
 import functions.Crypto;
 import org.junit.Test;
 import org.junit.Assert;
@@ -9,13 +12,41 @@ public class JUnitTest {
 
     @Test
     public void main(){
-        String string = "String to test crypto";
+        String string = "Here is a message to test packet";
+        String string_1 = "String to test crypto";
+
+        Message message = new Message(string, 0, 0);
+
+        Packet packet = new Packet(string, 0, 0, 0);
+        Packet packet_1 = new Packet("Message to check first packet", 1, 1, 1);
+        Packet packet_2 = new Packet("Message to check second packet", 2, 2, 2);
+
+        /** ========== Packet tests ========== **/
+
+        Assert.assertEquals(packet.getMessageLength(), message.getBytes().length);
+
+        /** ========== Packet receiver tests ========== **/
+
+        PacketReceiver packetReceiver_1 = new PacketReceiver(packet_1.getPacket());
+        PacketReceiver packetReceiver_2 = new PacketReceiver(packet_2.getPacket());
+
+        Assert.assertTrue(packetReceiver_1.getMessageValue().contains("first packet"));
+        Assert.assertTrue(packetReceiver_2.getMessageValue().contains("second packet"));
+
+        Assert.assertFalse(packetReceiver_1.getMessageValue().contains("Here is a message to test packet"));
+        Assert.assertFalse(packetReceiver_2.getMessageValue().contains("Here is a message to test packet"));
+
+        Assert.assertEquals(packet_1.getMessageLength(), packetReceiver_1.getMessageLength());
+        Assert.assertEquals(packet_2.getMessageLength(), packetReceiver_2.getMessageLength());
+
+        Assert.assertTrue(packetReceiver_1.isCorrect());
+        Assert.assertTrue(packetReceiver_2.isCorrect());
 
         /** ========== Crypto tests ========== **/
 
-        byte[] encrypted = Crypto.encrypt(string.getBytes(StandardCharsets.UTF_16BE));
+        byte[] encrypted = Crypto.encrypt(string_1.getBytes(StandardCharsets.UTF_16BE));
         byte[] decrypted = Crypto.decrypt(encrypted);
 
-        Assert.assertEquals(string, new String(decrypted, StandardCharsets.UTF_16BE));
+        Assert.assertEquals(string_1, new String(decrypted, StandardCharsets.UTF_16BE));
     }
 }
