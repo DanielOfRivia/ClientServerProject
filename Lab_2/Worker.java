c97d4a52620313e770592974d39d1dc314945940
DanielOfRivia <daniloustimenko@gmail.com>
2022-06-24 16:03:33 +0300

    Lab_2 Ustymenko

---
+++ Lab_2/Worker.java
@@ -0,0 +1,34 @@
+public class Worker extends Thread{
+
+    private int id;
+    private Data data;
+
+    public Worker(int id, Data d){
+        this.id = id;
+        data = d;
+        this.start();
+    }
+
+    @Override
+    public void run(){
+        super.run();
+        for (int i=0;i<5;i++) {
+            synchronized (data) {
+                try {
+                    while (id != data.getState()) {
+                        data.wait();
+                    }
+                    if (id == 1) {
+                        data.Tic();
+                    } else if (id == 2){
+                        data.Tak();
+                    } else data.Toi();
+                    data.notifyAll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
